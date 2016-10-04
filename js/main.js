65f4a71544df256e59147f9b421f71b0ec7117c1
Jester <marco.mascia@gmail.com>
2016-10-04 14:16:35 +0200

    autumn cleaning

--- js/main.js
+++
@@ -1,6 +0,0 @@
-var Muryoma = Muryoma || {};
-var game = new Phaser.Game(1024, 768, Phaser.CANVAS);
-game.state.add("BootState", new Muryoma.BootState());
-game.state.add("LoadingState", new Muryoma.LoadingState());
-game.state.add("ManageState", new Muryoma.ManageState());
-game.state.start("BootState", true, false, "assets/levels/manage.json", "ManageState");
